377986b500d960917930038fdcdea411f3f052fb
tetramputechture <tetramputechture@gmail.com>
2022-06-25 16:42:55 -0600

    Refactor kill process method to use array of process names

--- modules/server.py
+++ modules/server.py
@@ -5,6 +5,9 @@ for clients to utilize.
 author: Omar Barazanji (omar@omniaura.co)
 """
 
+import platform
+import omni_instance
+from constants import OMNISYNTH_PATH, SUPERCOLLIDER_PROCESS_NAMES
 import re
 import redis
 import subprocess
@@ -16,11 +19,8 @@ import json
 app = Flask(__name__)
 r = redis.Redis.from_url(url='redis://127.0.0.1:6379/0')
 
-from constants import OMNISYNTH_PATH
-import omni_instance
 OI = omni_instance.OmniInstance()
 
-import platform
 
 OS = 'Windows'
 if platform.system() == 'Linux':
@@ -29,27 +29,15 @@ elif platform.system() == 'Darwin':
     OS = 'Darwin'
 
 # postman as POST Test
+
+
 @app.route("/", methods=['POST'])
 def post_handler():
     return "<p>Hello, World!</p>"
 
-# making requests to the Flask Server instance
-@app.route("/server", methods=['POST', 'GET'])
-def omnisynth_handler():
-    requests = request.args
-
-    if request.method == "POST":
-        if '' in requests:
-            req = json.loads(requests['mapKnob'])
-            return f"<p>Mapping {knob_source, knob_chan} to {filter_name}</P"
-
-    if request.method == "GET":
-        if 'getKnobTable' in requests:
-            knob_table = r.get('knobTable')
-            return knob_table
-
-
 # making requests to the OmniSynth instance
+
+
 @app.route("/omnisynth", methods=['POST', 'GET'])
 def omnisynth_handler():
     requests = request.args
@@ -68,6 +56,7 @@ def omnisynth_handler():
             knob_table = r.get('knobTable')
             return knob_table
 
+
 @app.route("/patterns", methods=['POST', 'GET'])
 def patterns_handler():
     requests = request.args
@@ -79,7 +68,7 @@ def patterns_handler():
             OI.OmniSynth.pattern_sel(pattern, 'start')
             return f"<p>Starting {pattern}</p>"
 
-        elif 'stopPattern' in requests: 
+        elif 'stopPattern' in requests:
 
             pattern = str(request.args.get('stopPattern'))
             OI.OmniSynth.pattern_sel(pattern, 'stop')
@@ -98,7 +87,6 @@ def patterns_handler():
         pass
 
 
-
 @app.route("/patches", methods=['POST', 'GET'])
 def patches_handler():
     requests = request.args
@@ -108,12 +96,12 @@ def patches_handler():
             OI.compile_patches()
             return "<p>Compiling All Patches</p>"
 
-        elif 'patchName' in requests: 
+        elif 'patchName' in requests:
 
             patch = str(request.args.get('patchName'))
             OI.OmniSynth.synth_sel(patch, OMNISYNTH_PATH)
             return f"<p>Compiled {patch}</p>"
-        
+
         else:
 
             return "<p>Invalid Query. Must provide compileAllPatches or patchName.</p>"
@@ -125,7 +113,6 @@ def patches_handler():
     else:
 
         return "<p>Invalid Query. Must provide compileAllPatches or patchName.</p>"
-    
 
 
 # start supercollider server and simulates the GUI's event loop as `While True`.
@@ -135,20 +122,19 @@ def supercollider_handler():
     if request.method == "POST":
         if 'startServer' in requests:
             sc_main = OMNISYNTH_PATH + "main.scd"
-            if 'Darwin' in OS: subprocess.Popen(["/Applications/SuperCollider.app/Contents/MacOS/sclang", sc_main])
-            else: subprocess.Popen(["sclang", sc_main])
+            if 'Darwin' in OS:
+                subprocess.Popen(
+                    ["/Applications/SuperCollider.app/Contents/MacOS/sclang", sc_main])
+            else:
+                subprocess.Popen(["sclang", sc_main])
             return "<p>Starting server</p>"
 
         elif 'killServer' in requests:
 
-            OI.OmniSynth.exit_sel() # kills scsynth 
+            OI.OmniSynth.exit_sel()  # kills scsynth
             for proc in psutil.process_iter():
-                name = proc.name()
-                if 'sclang' in name.lower():
-                    print('killing sclang process...')
-                    proc.kill() # kills sclang
-                if 'scsynth' in name.lower():
-                    print('killing scsynth process...')
+                if proc.name().lower() in SUPERCOLLIDER_PROCESS_NAMES:
+                    print(f'killing process {name}')
                     proc.kill()
 
             return "<p>Server killed</p>"
@@ -156,7 +142,7 @@ def supercollider_handler():
         else:
 
             return "<p>Invalid Query. Must provide startServer or killServer.</p>"
-            
+
     elif request.method == "GET":
         if 'getOutDev' in requests:
             out_devices = r.get('outDevTable')
@@ -166,7 +152,6 @@ def supercollider_handler():
             return json.dump
 
 
-
 class Server():
 
     def __init__(self):
@@ -174,5 +159,5 @@ class Server():
 
 
 if __name__ == "__main__":
-    
+
     server = Server()
